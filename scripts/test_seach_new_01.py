20b6d4f848a5ea82aef95127adb381d11b233388
rjcschenshuai <chenshuai.srjl@foxmail.com>
2020-06-27 17:55:37 +0800

    新增代码

--- scripts/test_seach_new_01.py
+++ scripts/test_seach_new_01.py
@@ -5,7 +5,7 @@ from Page.searchPage import SearchPage
 from Base.driver import Driver
 from Base.get_date import Get_date
 
-
+import allure
 def gettt_date():
     date=Get_date.get_date('search_date.yaml')
     list = []
@@ -31,9 +31,10 @@ class Test_Search:
     def click_search_btn(self):
         """点击搜索按钮 并且 点击一次"""
         self.sp_obj.click_search_btn()
-
+    @allure.severity(allure.severity_level.BLOCKER)
+    @allure.step('总共分三步')
     @pytest.mark.parametrize("search_data, exp_data",date)
-    def test_search_text(self, search_data, exp_data):
+    def test_search_text_01(self, search_data, exp_data):
         """
         搜索测试方法
         :param search_data: 输入内容
@@ -42,5 +43,21 @@ class Test_Search:
         """
         # 输入框输入内容
         self.sp_obj.search_text(search_data)
+        allure.attach('测试输入框的','附件名字')
         # 断言
         assert exp_data in self.sp_obj.get_search_result()
+    @allure.severity(allure.severity_level.CRITICAL)
+    def test_02(self):
+        print('哈哈哈')
+
+    @allure.severity(allure.severity_level.NORMAL)
+    def test_03(self):
+        print('嘎嘎嘎嘎')
+
+    @allure.severity(allure.severity_level.MINOR)
+    def test_04(self):
+        print('嘻嘻嘻嘻')
+
+    @allure.severity(allure.severity_level.TRIVIAL)
+    def test_05(self):
+        prit('呵呵呵')
